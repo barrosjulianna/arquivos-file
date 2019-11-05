5d7f937ab70d912dee190b22a0af764089427821
Juliana Barros <juliana_barros2@hotmail.com>
2019-11-05 19:33:14 -0300

    Filereader e bufferedReader manual

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,31 +1,47 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
-
-
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		File file = new File("c:\\Users\\Zé\\Documents\\in.txt");
-		Scanner sc= null;
-		try {
-			sc = new Scanner(file);
-			while(sc.hasNextLine()){//SE EXISTE UMA LINHA NO ARQUIVO
-				System.out.println(sc.nextLine());
-				
-			}
-		}catch(IOException e){
-			System.out.println("Error: "+e.getMessage());
-		}
-		finally {
-			if(sc !=null)
-			sc.close();
-		}
-
+	String path = "c:\\Users\\Zé\\Documents\\in.txt";
+	FileReader fr=null;
+	BufferedReader br=null;
+	
+	try {
+		fr= new FileReader(path); //sequencia de leitura a partir desse caminho
+		br= new BufferedReader(fr); //deixa mais flexivel
+		
+		String line =br.readLine();
+		
+		while(line !=null) { //leu com sucesso a linha
+			System.out.println(line);
+			line= br.readLine();
 	}
+	
+			
+	}catch(IOException e) {
+		System.out.println("Error: " +e.getMessage());
+	}
+	finally {
+		try {
+			
+		
+			if(br!=null) {
+				br.close();
+			}
+			if(fr!=null) {
+				fr.close();
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 
+}
 }
